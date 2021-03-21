18f364dc8f1dbb1c41703a8a608e22c0dcf478c1
shonjiho <jihoson94@gmail.com>
2021-03-21 11:57:19 +0900

    apply base layout style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Discord Clone</div>;
+  return (
+    <div className="app-container">
+      <div className="navbar">NavBar</div>
+      <div className="leftmenu">LeftMenu</div>
+      <div className="chat">Chat</div>
+      <div className="rightmenu">RightMenu</div>
+    </div>
+  );
 }
 
 export default App;
