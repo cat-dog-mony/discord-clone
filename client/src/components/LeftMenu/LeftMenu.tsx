98fbec4054a3a201f27107a2ec61181de5aa2ef8
shonjiho <jihoson94@gmail.com>
2021-03-24 10:39:31 +0900

    left menu half

--- client/src/components/LeftMenu/LeftMenu.tsx
+++ client/src/components/LeftMenu/LeftMenu.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { BiChevronDown, BiPlus, BiX, BiChevronRight } from "react-icons/bi";
+
 import "./LeftMenu.css";
 
 const LeftMenu = () => {
-  return <div className="leftmenu"></div>;
+  const [showTitleMenu, setShowTitleMenu] = useState(false);
+  const handleTitleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setShowTitleMenu(!showTitleMenu);
+  };
+  return (
+    <div className="leftmenu">
+      <div className="title-container" onClick={handleTitleClick}>
+        <div className="title">Server Name</div>
+        <div className="title-icon">
+          {showTitleMenu ? <BiChevronDown /> : <BiX />}
+        </div>
+      </div>
+      <div className="channel-section-header">
+        <BiChevronRight className="channel-section-arrow" />
+        <span>채팅 채널</span>
+        <BiPlus className="channel-plus-btn" />
+      </div>
+      <ul className="channel-list">
+        <li className="channel-list-item">
+          <div></div>
+          <div></div>
+          <div></div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default LeftMenu;
