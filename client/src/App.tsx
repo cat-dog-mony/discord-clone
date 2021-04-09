800ce9870494f1a07216622a87c0e77bc6e0814f
jihoson94 <jihoson94@rnd.re.kr>
2021-04-09 22:52:23 +0900

    Apply React Router DOM

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-import Chat from "./components/Chat/Chat";
-import LeftMenu from "./components/LeftMenu/LeftMenu";
-import Navbar from "./components/Navbar/Navbar";
-import RightMenu from "./components/RightMenu/RightMenu";
-import styled from "styled-components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MainPage from "components/pages/MainPage";
+import AuthPage from "components/pages/AuthPage";
 
 function App() {
   return (
-    <AppContainer>
-      <Navbar />
-      <LeftMenu />
-      <Chat />
-      <RightMenu />
-    </AppContainer>
+    <Router>
+      <Switch>
+        <Route path="/auth">
+          <AuthPage />
+        </Route>
+        <Route path="/">
+          <MainPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-const AppContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  height: 100vh;
-`;
 
 export default App;
