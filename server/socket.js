39c91ad5a13809ea08ba5595071817fecaa91f8f
wonyoung <onezerokang@gmail.com>
2021-04-16 12:56:19 +0900

    [DC-25] connect db in socket.js

--- server/socket.js
+++ server/socket.js
@@ -2,67 +2,63 @@ const SocketIo = require("socket.io");
 const Channel = require("./models/Channel");
 const Message = require("./models/Message");
 
-module.exports = (server, app) => {
+module.exports = (server) => {
   const io = SocketIo(server);
-  app.set("io", io);
 
   io.on("connection", (socket) => {
     socket.on("test", (data) => {
-      console.log(data)
-      io.emit('chat', data );
-    })
-    socket.on('chat', (data) => {
-      console.log(data)
-      socket.broadcast.emit('chat', 'test chat')
-    })
-    socket.on("join", async ({ joinData }, callback) => {
+      console.log(data);
+    });
+
+    socket.on("join", async (joinData, callback) => {
       try {
         const channelId = joinData.channel;
         const userId = joinData.user;
-        const channel = await Channel.findByIdAndUpdate(
-          { _id: channelId },
-          { $push: { user: userId } }
-        )
-          .populate("user")
-          .exec();
-        await channel.save();
+        const channel = await Channel.userJoinChannel(channelId, userId);
         socket.join(channel._id);
         io.to(channel._id).emit("message", {
           user: "system",
           text: `앗! 야생의 ${channel.user._id}이(가) 나타났다!`,
         });
-        callback();
+        callback(channel);
       } catch (error) {
         callback(error);
       }
     });
-    socket.on("sendMessage", async ({ chatroomId, message }) => {
-      const user = await User.findOne({ _id: socket.userId });
+
+    socket.on("sendMessage", async (messageData) => {
+      const user = await User.findOne({ _id: socket.id });
       const newMessage = new Message({
-        chatroom: chatroomId,
-        user: socket.userId,
-        message,
+        chatroom: messageData.channelId,
+        //배열안에 들어있어야 하는데 이게 맞나?
+        user: socket.id,
+        content: messageData.content,
       });
-      io.to(chatroomId).emit("newMessage", {
-        message,
+      io.to(messageData.channelId).emit("newMessage", {
+        content: messageData.content,
         name: user.name,
-        userId: socket.userId,
+        id: socket.id,
       });
       await newMessage.save();
     });
+
     socket.on("leaveRoom", async (joinData, callback) => {
       try {
-        const channel = await Channel.findOneAndDelete({
-          _id: joinData.channel,
-          user: joinData.user,
-        });
+        const channel = await Channel.findOneAndUpdate(
+          {
+            _id: joinData.channel,
+          },
+          {
+            $pull: { user: userId },
+          }
+        );
         await channel.save();
-        socket.leave(channel._id);
-        io.to(channel._id).emit("message", {
+        socket.leave(joinData.channel);
+        io.to(joinData.channel).emit("message", {
           user: "system",
           text: `${joinData.user}가 나갔습니다.`,
         });
-        callback();
+        callback(channel);
       } catch (error) {
         callback(error);
       }
